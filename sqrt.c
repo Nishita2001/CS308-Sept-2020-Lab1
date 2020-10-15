d7ee07dc6f66093b0308f667e26da6fed2e22a74
Nishita2001 <nishita300601@gmail.com>
2020-10-15 23:18:52 +0530

    changing printf statements to observe conflicts

--- sqrt.c
+++ sqrt.c
@@ -25,11 +25,11 @@ int main(int argc, char* argv[]) {
 		//modifications to find square root of negative numbers have been added
 	}
 	if(input<0){
-		printf("NEGATIVE SUPPORT : Sqrt of %d is %fi\n",input,sqrt(-input));
+		printf("DEBUG : Sqrt of %d is %fi\n",input,sqrt(-input));
 	}
 	else
-	printf("NEGATIVE SUPPORT : Sqrt of %d is %f\n",input,sqrt(input));
-	printf("NEGATIVE SUPPORT : End of program...Exiting...\n");
+	printf("DEBUG : Sqrt of %d is %f\n",input,sqrt(input));
+	printf("DEBUG : End of program...Exiting...\n");
 	return(0);
 
 } // end main
