8726f10fc6ccce3c5588fa55ffec7d11703684b9
Nishita2001 <nishita300601@gmail.com>
2020-10-15 23:09:47 +0530

    making changes to this branch to see conflicts

--- sqrt.c
+++ sqrt.c
@@ -25,11 +25,11 @@ int main(int argc, char* argv[]) {
 		//modifications to find square root of negative numbers have been added
 	}
 	if(input<0){
-		printf("Sqrt of %d is %fi\n",input,sqrt(-input));
+		printf("NEGATIVE SUPPORT : Sqrt of %d is %fi\n",input,sqrt(-input));
 	}
 	else
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("End of program...Exiting...\n");
+	printf("NEGATIVE SUPPORT : Sqrt of %d is %f\n",input,sqrt(input));
+	printf("NEGATIVE SUPPORT : End of program...Exiting...\n");
 	return(0);
 
 } // end main
